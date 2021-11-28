cf9794d9dd811cfaa58d7dfa13baee14c133cf12
Jrb <jrb.youssef@gmail.com>
2021-11-28 22:29:12 +0100

    Create DouLink

---
+++ DouLink/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Nuxt-link :to="to" class="text-purple tracking-wider">
+    {{ text }}
+  </Nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'DouLink',
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
