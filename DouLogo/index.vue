f7c87254e8760e4f825b49b516fe3b4af5f3d9d8
Jrb <jrb.youssef@gmail.com>
2021-11-28 17:02:21 +0100

    Create DouLogo

---
+++ DouLogo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <Nuxt-link to="/">
+    <img
+      :class="`w-${width}`"
+      :src="logo_light"
+      class="hidden sm:block"
+    />
+    <img
+      :class="`w-${width}`"
+      :src="logo_dark"
+      class="block sm:hidden"
+    />
+  </Nuxt-link>
+</template>
+<script>
+  export default {
+    name: 'DouLogo',
+    props: {
+      width: {
+        default: 24,
+      }
+    },
+    data () {
+      return {
+        logo_light: require('~/assets/image/logo-light.png'),
+        logo_dark: require('~/assets/image/logo-dark.png'),
+      }
+    }
+  }
+</script>
--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Dourou Components Library
 -------
 
-1. DouButton
-2. DouInput
-3. DouLabel
-4. DouHref
-
 **TODO**
-1. DouCard
-2. DouContainer
-3. DouLogo
-4. DouBanner
-5. DouDrawer
-6. DouPhoneInput
+- [X] DouButton
+- [X] DouInput
+- [X] DouLabel
+- [X] DouHref
+- [X] DouLogo
+  - [ ] Sortir le require des images du composant.
+- [ ] DouCard
+- [ ] DouContainer
+- [ ] DouBanner
+- [ ] DouDrawer
+- [ ] DouPhoneInput
