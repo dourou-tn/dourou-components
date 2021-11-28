00feb3f316939fa09275cf0eb870bc2511199ff1
Jrb <jrb.youssef@gmail.com>
2021-11-28 15:35:10 +0100

    Create dou-href

---
+++ DouHref/index.vue
@@ -0,0 +1,23 @@
+<template>
+<a
+  v-text="text"
+  :href="href"
+  v-on="$listeners"
+  class="inline-block align-baseline text-sm text-indigo-500 hover:text-indigo-800"
+/>
+</template>
+<script>
+export default {
+  name: 'DouHref',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      default: '#',
+    }
+  }
+}
+</script>
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 1. DouButton
 2. DouInput
 3. DouLabel
+4. DouHref
 
 **TODO**
 1. DouCard
@@ -11,3 +12,4 @@
 3. DouLogo
 4. DouBanner
 5. DouDrawer
+6. DouPhoneInput
