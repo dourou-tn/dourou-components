f15aeafab78f41591234055c1bcf1ce65a1349be
Jrb <jrb.youssef@gmail.com>
2021-12-03 22:10:09 +0100

    Add error to Input

--- DouInput/index.vue
+++ DouInput/index.vue
@@ -3,6 +3,7 @@
     <DouLabel v-if="label" :label="label" />
     <input
       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      :class="{ 'border-red-300': error }"
       :id="id"
       :type="type"
       :placeholder="placeholder"
@@ -11,6 +12,7 @@
       :value="value"
     />
     <p v-if="hint" v-text="hint" class="text-gray-500 text-xs mt-2" />
+    <p v-if="error" v-text="error" class="text-red-500 text-xs mt-2" />
   </div>
 </template>
 
@@ -37,6 +39,10 @@ export default {
     },
     hint: {
       type: String,
+    },
+    error: {
+      type: String,
+      default: '',
     }
   },
   computed: {
