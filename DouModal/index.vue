d405d626f7ab22df064a6ba73e8be8fefc90c3e9
Jrb <jrb.youssef@gmail.com>
2021-12-10 00:59:45 +0100

    Create DouModal

---
+++ DouModal/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="modal" :class="show ? 'modal-open' : ''">
+  <div
+    class="modal-box max-w-xl"
+  >
+    <slot />
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'DouModal',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    }
+  }
+}
+</script>
