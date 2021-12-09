8f389ab229b471caf8a78235d7fe0401520e0846
Jrb <jrb.youssef@gmail.com>
2021-12-10 00:59:58 +0100

    Create DouSelect

---
+++ DouSelect/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- custom select component  -->
+  <select
+    v-model="value"
+    v-on="$listeners"
+    @change="change"
+    class="select select-bordered select-primary w-full"
+  >
+    <option
+      v-for="item in items"
+      :value="item.value"
+      :key="item.id"
+    >
+      {{ item.label }}
+    </option> 
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'DouSelect',
+  props: {
+    value: {},
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    change(e) {
+      this.$emit('change', e.target.value)
+    }
+  },
+}
+</script>
