4cf93ab6b31bd3a5662d1a9fe9214ff5f987e024
Jrb <jrb.youssef@gmail.com>
2021-12-10 01:00:09 +0100

    Cretea DouTable

---
+++ DouTable/index.vue
@@ -0,0 +1,128 @@
+<template>
+
+  <div class="overflow-x-auto">
+    <table class="table w-full table-compact">
+      <thead>
+        <tr >
+          <th
+            v-for="col in columns"
+            :key="col.field"
+            class="hover"
+          >
+            {{ col.title }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="item.id" class="hover">
+            <td
+              v-for="(col, index) in columns" :key="col.field">
+              {{ item[col.field] }}
+            </td>
+        </tr>
+      </tbody> 
+      <!-- <tfoot>
+        <tr>
+          <th
+            v-for="col in columns"
+            :key="col.field">
+            {{ col.title }}
+          </th>
+        </tr>
+      </tfoot> -->
+    </table>
+  </div>
+
+  <!-- <div class="">
+    <div class="my-2 flex sm:flex-row flex-col">
+        <div class="flex flex-row mb-1 sm:mb-0">
+            <div class="">
+                <select
+                    class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                    <option>5</option>
+                    <option>10</option>
+                    <option>20</option>
+                </select>
+                <div
+                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="block  flex-1">
+            <input placeholder="Search"
+                class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+        </div>
+    </div>
+    <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4">
+        <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <table class="min-w-full leading-normal">
+                <thead>
+                    <tr>
+                        <th
+                          v-for="col in columns"
+                          :key="col.field"
+                          class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          {{ col.title }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in items" :key="item.id">
+                        <td 
+                          v-for="(col, index) in columns" :key="col.field"
+                        class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <div class="flex items-center">
+                                <div >
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ item[col.field] }}
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div
+                class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                <span class="text-xs xs:text-sm text-gray-900">
+                    Showing 1 to 4 of 50 Entries
+                </span>
+                <div class="inline-flex mt-2 xs:mt-0">
+                    <button
+                        class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                        Prev
+                    </button>
+                    <button
+                        class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                        Next
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div> -->
+</template>
+<script>
+export default {
+  name: 'DouTable',
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    columns: {
+      type: Array,
+      default: () => [{ field: '', title: 'column' }]
+    }
+  },
+  methods: {
+    getData (field, index) {
+      console.log(field, index)
+      return this.items[index][field];
+    }
+  }
+}
+</script>
